c9f21f1792c199e82dd0f02b26c70ff75c964244
Pavel Pecherskih <pecherskih@netcracker.com>
2022-06-18 22:48:58 +0400

    add hometask

---
+++ task_021/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход координаты 
+двух точек и находит расстояние между ними
+ в 3D пространстве.
+ */
+
+Random rand = new Random();
+int x1 = rand.Next (0,10);
+int y1 = rand.Next (0,10);
+int z1 = rand.Next (0,10);
+int x2 = rand.Next (0,10);
+int y2 = rand.Next (0,10);
+int z2 = rand.Next (0,10);
+double res = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+Console.WriteLine($"{res} расстояние между двумя точками");
+
---
+++ task_023/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход число (N)
+ и выдаёт таблицу кубов чисел от 1 до N.
+ */
+
+Random rand = new Random();
+int n = rand.Next(1,10);
+int i = 1;
+while (i <= n)
+{
+Console.WriteLine($"{i} --> {Math.Pow(i, 3)}");
+i++;
+}
